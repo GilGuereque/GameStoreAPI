2e9cc2c21c9a30b27bab1ee6b670a34165239e8f
Gilberto Guereque <gguereque326@gmail.com>
2024-04-20 17:19:17 -0700

    initial GET call http request

--- GameStore/obj/Debug/net8.0/GameStore.AssemblyInfo.cs
+++ GameStore/obj/Debug/net8.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+784e46909d48e09ee8fc2b09bc5ed2cdc1686378")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7953d568252eaa9e3ba9913e3fe1ab9b8a268782")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
