2ed6ef6043ac6d854868e82d35886ac7aa77d3c1
GilGuereque <gguereque326@gmail.com>
2024-04-25 14:26:07 -0700

    hardcoded connection string to a variable and added Service for the DBcontext file

--- GameStore/Program.cs
+++ GameStore/Program.cs
@@ -1,7 +1,13 @@
 // Entry point file for the GameStore API
+using GameStore.Data;
 using GameStore.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// DB Connection String
+var connString = "Data Source=GameStore.db";
+//Add services for the GameStoreContext and connecting to the DB file with the conn string (utilizies Dependency Injection)
+builder.Services.AddSqlite<GameStoreContext>(connString); 
 
 var app = builder.Build();
 
