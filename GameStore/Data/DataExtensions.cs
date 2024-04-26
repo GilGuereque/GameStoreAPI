4e9687767bcc96b5bc5f389b32d1d52fad76acb2
Gilberto Guereque <gguereque326@gmail.com>
2024-04-26 16:43:34 -0700

    Updated DataExtensions method to be asynchronous

--- GameStore/Data/DataExtensions.cs
+++ GameStore/Data/DataExtensions.cs
@@ -4,10 +4,10 @@ namespace GameStore.Data;
 
 public static class DataExtensions
 {
-    public static void MigrateDB(this WebApplication app)
+    public static async Task MigrateDBAsync(this WebApplication app)
     {
         using var scope = app.Services.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
-        dbContext.Database.Migrate();
+        await dbContext.Database.MigrateAsync();
     }
 }
--- GameStore/Program.cs
+++ GameStore/Program.cs
@@ -13,6 +13,6 @@ var app = builder.Build();
 
 app.MapGamesEndpoints();
 
-app.MigrateDB();
+await app.MigrateDBAsync();
 
 app.Run();
