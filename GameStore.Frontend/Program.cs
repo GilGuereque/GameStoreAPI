8e5c1471f191b8a299fd52a4d3ee20efc04ee4bf
GilGuereque <gguereque326@gmail.com>
2024-04-27 12:13:48 -0700

    Created new Blazor frontend application to connect to our GameStore.API backend

---
+++ GameStore.Frontend/Program.cs
@@ -0,0 +1,31 @@
+using GameStore.Frontend.Components;
+
+// Create our application host which is WebApplication
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container. (using dependency injection)
+builder.Services.AddRazorComponents();
+
+var app = builder.Build();
+
+// Middleware
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) //if we are not in dev, so production
+{
+    // Use UseExceptionHandler to send an Error to the user if something goes wrong in the app
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// Set application up to use HTTPS if needed (we won't be using this)
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+// Configure middleware to be able to discover all the Razor components we created for our app
+app.MapRazorComponents<App>();
+
+// Lastly invoke Run to start our application and enable it to start receiving requests
+app.Run();
