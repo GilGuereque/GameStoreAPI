459a1b0f295b49d177402407c2da2a97402d124f
GilGuereque <gguereque326@gmail.com>
2024-05-03 18:30:58 -0700

    Added New Game button routing to the edit game page from the Home page

--- GameStore.Frontend/Clients/GamesClient.cs
+++ GameStore.Frontend/Clients/GamesClient.cs
@@ -5,30 +5,30 @@ namespace GameStore.Frontend.Clients;
 public class GamesClient
 {
     // Changed GameSummary to a List as it is dynamic changing array instead
-    private readonly List<GameSummary> games =
-    [
-        new(){
+    private readonly List<GameSummary> games = new List<GameSummary>
+    {
+        new GameSummary {
             Id = 1,
             Name = "Street Fighter II",
             Genre = "Fighting",
             Price = 19.99M,
             ReleaseDate = new DateOnly(1992, 7, 15)
         },
-        new(){
+        new GameSummary {
             Id = 2,
             Name = "Final Fantasy XIV",
             Genre = "Roleplaying",
             Price = 59.99M,
             ReleaseDate = new DateOnly(2010, 9, 30)
         },
-        new(){
+        new GameSummary {
             Id = 3,
             Name = "FIFA 23",
             Genre = "Sports",
             Price = 69.99M,
             ReleaseDate = new DateOnly(2022, 9, 27)
         }
-    ];
+    };
 
     private readonly Genre[] genres = new GenresClient().GetGenres();
 
@@ -36,19 +36,21 @@ public class GamesClient
 
     public void AddGame(GameDetails game)
     {
-        ArgumentException.ThrowIfNullOrWhiteSpace(game.GenreId); // throw exception if null Id
+        if (game.GenreId == null)
+            throw new ArgumentException("Genre ID cannot be null");
+            // ArgumentException.ThrowIfNullOrWhiteSpace(game.GenreId); // throw exception if null Id
 
-        var genre = genres.Single(genre => genre.Id == int.Parse(game.GenreId));
+            var genre = genres.Single(genre => genre.Id == int.Parse(game.GenreId.Value.ToString()));
 
-        var gameSummary = new GameSummary
-        {
-            Id = games.Count + 1,
-            Name = game.Name,
-            Genre = genre.Name,
-            Price = game.Price,
-            ReleaseDate = game.ReleaseDate
-        };
+            var gameSummary = new GameSummary
+            {
+                Id = games.Count + 1,
+                Name = game.Name,
+                Genre = genre.Name,
+                Price = game.Price,
+                ReleaseDate = game.ReleaseDate
+            };
 
-        games.Add(gameSummary);
+            games.Add(gameSummary);
     }
 }
--- GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
+++ GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e46b9452e0a67831770e64cd754bc4438c22cebf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07f29a89bcd6ae5723d7b3e3b4dcd89d67892086")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
