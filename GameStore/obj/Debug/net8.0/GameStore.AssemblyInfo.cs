07f29a89bcd6ae5723d7b3e3b4dcd89d67892086
GilGuereque <gguereque326@gmail.com>
2024-05-03 17:19:26 -0700

    Added submit button and HandleSubmit method to the Edit form

--- GameStore/obj/Debug/net8.0/GameStore.AssemblyInfo.cs
+++ GameStore/obj/Debug/net8.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8640916761d43ad105192ce24405b6e14e1326f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea589ca091305f58e7286237f06dcb45e28f986b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
