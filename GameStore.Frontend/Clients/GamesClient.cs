f6aa37fc5f71d79d897633b31b103eb415e0c31b
GilGuereque <gguereque326@gmail.com>
2024-05-11 16:28:36 -0700

    Updated GamesClient

--- GameStore.Frontend/Clients/GamesClient.cs
+++ GameStore.Frontend/Clients/GamesClient.cs
@@ -53,4 +53,24 @@ public class GamesClient
 
             games.Add(gameSummary);
     }
+
+    public GameDetails GetGame(int id)
+    {
+        GameSummary? game = games.Find(game => game.Id == id);
+        ArgumentNullException.ThrowIfNull(game);
+
+        var genre = genres.Single(genre => string.Equals(
+            genre.Name,
+            game.Genre,
+            StringComparison.OrdinalIgnoreCase));
+
+        return new GameDetails
+        {
+            Id = game.Id,
+            Name = game.Name,
+            GenreId = genre.Id.ToString(),
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate
+        };
+    }
 }
