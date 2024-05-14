44a535471d52329a260d42efc7acae3fabd2de96
GilGuereque <gguereque326@gmail.com>
2024-05-13 19:36:12 -0700

    updated files

--- GameStore/obj/Debug/net8.0/GameStore.AssemblyInfo.cs
+++ GameStore/obj/Debug/net8.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6aa37fc5f71d79d897633b31b103eb415e0c31b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3572d5ec563f0b1908952278d79e9b5badee127")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
